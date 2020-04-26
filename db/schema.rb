ae95e6c4afff844f168df76f67a7ebca648990d6
松本和奈 <matsumotokazuna@matsumotowananoMacBook-Pro.local>
2020-04-26 18:54:47 +0900

    [add]topic7

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_19_073432) do
+ActiveRecord::Schema.define(version: 2020_04_25_122654) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,21 @@ ActiveRecord::Schema.define(version: 2020_04_19_073432) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
+
+  create_table "user_rooms", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "room_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_user_rooms_on_room_id"
+    t.index ["user_id"], name: "index_user_rooms_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
